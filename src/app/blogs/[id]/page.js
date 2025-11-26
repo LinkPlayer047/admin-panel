32eaea158f67ae05814ec65d6be50626ef7a6ede
LinkPlayer047 <linkplayer047@gmail.com>
2025-11-26 16:47:44 +0500

    ok

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -1,6 +1,6 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import Navbar from "../../components/Navbar";
+import Navbar from "../../../components/Navbar";
 
 const EditBlog = ({ params }) => {
   const blogId = params.id;
