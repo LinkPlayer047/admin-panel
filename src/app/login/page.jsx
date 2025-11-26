e751adaeac6ddac96019d1327b64c4ee6a630730
LinkPlayer047 <linkplayer047@gmail.com>
2025-11-26 17:19:36 +0500

    ok

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch("http://localhost:5000/api/login", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username: email, password }),
