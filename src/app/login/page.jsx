d27a61d7750dcae02d97e34b2b9c37753ab06b6b
LinkPlayer047 <linkplayer047@gmail.com>
2025-11-27 14:50:41 +0500

    ok

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -14,7 +14,7 @@ const LoginPage = () => {
     setError("");
 
     try {
-      const res = await fetch("https://web-solutions-backend.vercel.app/api/login", {
+      const res = await fetch("http://localhost:5000/api/login", "https://web-solutions-backend.vercel.app/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
