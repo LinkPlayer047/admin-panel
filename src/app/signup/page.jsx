5abf8925f2e099f6cd7d568c1575539b93141e06
LinkPlayer047 <linkplayer047@gmail.com>
2025-11-27 13:08:48 +0500

    ok

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -26,7 +26,8 @@ const SignupPage = () => {
       const res = await fetch("https://web-solutions-backend.vercel.app/api/sendcode", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email }),
+        body: JSON.stringify({ email: userEmail }),
+        credentials: "include",
       });
 
       const data = await res.json();
