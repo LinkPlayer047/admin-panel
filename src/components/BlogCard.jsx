2ea79f0c2a9b03632a944549fa41977dc8094c34
LinkPlayer047 <linkplayer047@gmail.com>
2025-12-04 18:10:14 +0500

    ok

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,19 +1,14 @@
 export default function BlogCard({ blog, onEdit, onDelete }) {
   return (
-    <div className="border rounded shadow bg-white overflow-hidden hover:shadow-lg transition">
+    <div className="border rounded shadow bg-white hover:shadow-lg transition">
       <img src={blog.image} className="h-48 w-full object-cover rounded-t" />
 
       <div className="p-4">
         <h2 className="font-bold text-lg">{blog.title}</h2>
         <p className="text-gray-600">{blog.subtitle}</p>
 
-        <p className="mt-2 text-gray-700"
-           style={{
-           display: "-webkit-box",
-           WebkitLineClamp: 2,
-           WebkitBoxOrient: "vertical",
-           overflow: "hidden"}} >
-           {blog.content}
+        <p className="mt-2 truncate overflow-hidden text-gray-700">
+          {blog.content.split(" ").slice(0, 20).join(" ")}...
         </p>
 
         <div className="flex justify-between mt-3">
