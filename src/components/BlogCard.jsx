c7d6a6cef55d0d35071c2c29f8c18f2533972f29
LinkPlayer047 <linkplayer047@gmail.com>
2025-12-04 18:39:03 +0500

    ok

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,13 +1,13 @@
 export default function BlogCard({ blog, onEdit, onDelete }) {
   return (
-    <div className="border rounded shadow bg-white hover:shadow-lg transition">
+    <div className="border rounded shadow bg-white truncate overflow-hidden hover:shadow-lg transition">
       <img src={blog.image} className="h-48 w-full object-cover rounded-t" />
 
       <div className="p-4">
         <h2 className="font-bold text-lg">{blog.title}</h2>
         <p className="text-gray-600">{blog.subtitle}</p>
 
-        <p className="mt-2 truncate overflow-hidden text-gray-700">
+        <p className="mt-2 text-gray-700">
           {blog.content.split(" ").slice(0, 20).join(" ")}...
         </p>
 
