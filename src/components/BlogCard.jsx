873b2f94a140ebce6a877dd59f2b9c52d3e5f1d3
LinkPlayer047 <linkplayer047@gmail.com>
2025-12-05 18:15:22 +0500

    OOK

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,22 +1,30 @@
 export default function BlogCard({ blog, onEdit, onDelete }) {
   return (
-    <div className="border rounded shadow bg-white truncate overflow-hidden hover:shadow-lg transition">
+    <div className="border rounded shadow bg-white overflow-hidden hover:shadow-lg transition">
       <img src={blog.image} className="h-48 w-full object-cover rounded-t" />
 
       <div className="p-4">
         <h2 className="font-bold text-lg">{blog.title}</h2>
         <p className="text-gray-600">{blog.subtitle}</p>
 
-        <p className="mt-2 line-clamp-2 leading-relaxed text-gray-700">
-          {blog.content.split(" ").slice(0, 20).join(" ")}...
+        <p className="mt-2 leading-relaxed text-gray-700 line-clamp-2">
+          {blog.content}
         </p>
 
         <div className="flex justify-between mt-3">
-          <button className="bg-yellow-500 text-white px-3 py-1 rounded"
-            onClick={onEdit}>Edit</button>
+          <button
+            className="bg-yellow-500 text-white px-3 py-1 rounded"
+            onClick={onEdit}
+          >
+            Edit
+          </button>
 
-          <button className="bg-red-500 text-white px-3 py-1 rounded"
-            onClick={onDelete}>Delete</button>
+          <button
+            className="bg-red-500 text-white px-3 py-1 rounded"
+            onClick={onDelete}
+          >
+            Delete
+          </button>
         </div>
       </div>
     </div>
